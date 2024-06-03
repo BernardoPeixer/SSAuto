4f853a2a30aca09b5fcaf470938b79dc26e1ba4e
BernardoPeixer <bernardo.peixer.stahelin@gmail.com>
2024-06-03 15:51:19 -0300

    Criacao da tela de cadastro de gerente

--- lib/view/managerRegistration.dart
+++ lib/view/managerRegistration.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ss_auto/view/widgets/textFormFieldVehicles.dart';
 
 class ManagerRegistration extends StatelessWidget {
   const ManagerRegistration({super.key});
@@ -24,11 +25,49 @@ class ManagerRegistration extends StatelessWidget {
         ),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          const Icon(
+            Icons.person,
+            size: 150,
+            color: Colors.white,
+          ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text('data'),
+              TextFormFieldVehicles(title: 'Nome Completo:', color: orange),
+              TextFormFieldVehicles(title: 'Cidade:', color: orange),
             ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextFormFieldVehicles(title: 'CPF:', color: orange),
+              TextFormFieldVehicles(title: 'Estado:', color: orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextFormFieldVehicles(title: 'E-mail::', color: blu),
+              TextFormFieldVehicles(title: 'Telefone', color: blu),
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.5,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(orange),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Salvar',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
           )
         ],
       ),
